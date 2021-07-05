696a108c1ed8655f294df7ae6d888208d244c0d0
aydenvis <38897937+aydenvis@users.noreply.github.com>
2021-07-05 14:48:03 -0500

    Updated to QMK Ver 13.17 and added default hexfile

--- QMK/threetoone/keymaps/default/keymap.c
+++ QMK/threetoone/keymaps/default/keymap.c
@@ -1,33 +1,52 @@
+/* Copyright 2021 Ayden Visser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
-
-#define _BASE 0
 
 uint8_t left_encoder_layer = 0; // A variable used to alter encoder behaviour.
 uint8_t right_encoder_layer = 0; // A variable used to alter encoder behaviour.
 uint8_t encoder_layer_count = 2; // The amount of encoder layers
 
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+// Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LENC,
-  RENC,
+    QWERTY = SAFE_RANGE,
+    LENC,
+    RENC,
  };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-[_BASE] = LAYOUT( \
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,             KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS, RENC, \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,              KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,              KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_P7,   KC_P8,   KC_P9,           \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,              KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,           KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_P1,   KC_P2,   KC_P3,            \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           LENC,             KC_SPC,  KC_RALT,           KC_RGUI, KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT  \
-)
+    /* Base */
+    [_BASE] = LAYOUT(
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,             KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS, RENC,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,              KC_9,    KC_0,    KC_MINS, KC_BSPC, KC_EQL, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,              KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_P7,   KC_P8,   KC_P9,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,              KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,           KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_P1,   KC_P2,   KC_P3, 
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           LENC,             KC_SPC,  KC_RALT,           KC_RGUI, KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
+  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) 
-  {
+    switch (keycode) 
+    {
     case LENC:
       if (record->event.pressed)
       {
@@ -55,12 +74,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       {
       }
       break;
-  }
-  return true;
+    }
+    return true;
 };
 
 //Encoder
-void encoder_update_user(uint8_t index, bool clockwise)
+bool encoder_update_user(uint8_t index, bool clockwise)
 {
   switch(index)
   {
@@ -108,14 +127,15 @@ void encoder_update_user(uint8_t index, bool clockwise)
         case 1: // Layer 0
           if (clockwise) 
           {
-            tap_code(KC_VOLU);
+            tap_code(KC_PGUP);
           } 
           else 
           {
-            tap_code(KC_VOLD);
+            tap_code(KC_PGDN);
           }
           break;
       }
       break;
   }
+  return false;
 }
